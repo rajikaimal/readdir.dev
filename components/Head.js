4e96c3218a4dbee71b31fc8c2f9331a5f527153a
rajikaimal <inbox.rajika@gmail.com>
2021-05-15 14:48:41 +0530

    Add missing Twitter tags

--- components/Head.js
+++ components/Head.js
@@ -8,8 +8,20 @@ const RuntimeHead = (props) => {
       <meta property="og:type" content="website" />
       <meta property="og:title" content={props.title && `- ${props.title}`} />
       <meta property="og:image" content="https://theruntime.xyz/meta-img.png" />
-      <meta property="twitter:image" content="https://theruntime.xyz/meta-img.png" />
+
+      {/* Twitter tags */}
       <meta name="twitter:card" content={props.title && `- ${props.title}`} />
+      <meta name="twitter:title" content={props.title} />
+      <meta name="twitter:description" content={props.title} />
+      <meta
+        property="twitter:image"
+        content="https://theruntime.xyz/meta-img.png"
+      />
+      <meta
+        name="twitter:image"
+        content=" http://euro-travel-example.com/thumbnail.jpg"
+      />
+      <meta name="twitter:card" content="summary" />
     </Head>
   );
 };
