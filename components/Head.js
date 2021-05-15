5855acd75be978f3002b393bde3f608d3692a754
rajikaimal <inbox.rajika@gmail.com>
2021-05-15 15:45:24 +0530

    Remove duplicate tag

--- components/Head.js
+++ components/Head.js
@@ -10,14 +10,13 @@ const RuntimeHead = (props) => {
       <meta property="og:image" content="https://theruntime.xyz/meta-img.png" />
 
       {/* Twitter tags */}
-      <meta name="twitter:card" content={props.title && `- ${props.title}`} />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={props.title} />
       <meta name="twitter:description" content={props.title} />
       <meta
         name="twitter:image:src"
         content="https://theruntime.xyz/meta-img.png"
       />
-      <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
 };
