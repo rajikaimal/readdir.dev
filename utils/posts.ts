a9bf9fabc422b8057eb7d02e8ebe2dbac82b468d
rajikaimal <inbox.rajika@gmail.com>
2024-12-25 11:48:36 +0100

    fix front-matter parsing

--- utils/posts.ts
+++ utils/posts.ts
@@ -1,34 +1,35 @@
-import FrontMatter from "@burmajs/frontmatter";
 import { join } from "$std/path/join.ts";
+import frontMatter from "https://esm.sh/front-matter@4.0.2";
 
 export interface Post {
-    slug: string;
-    title: string;
-    publishedAt: Date;
-    content: string;
-    snippet: string;
+  slug: string;
+  title: string;
+  publishedAt: Date;
+  content: string;
+  snippet: string;
 }
 
 export async function getPost(slug: string): Promise<Post | null> {
-    const text = await Deno.readTextFile(join("./posts", `${slug}.md`));
-    const { json } = new FrontMatter<Post>(text);
-    return {
-        slug,
-        title: json.data.title,
-        publishedAt: new Date(json.data.publishedAt),
-        content: json.content,
-        snippet: json.snippet,
-    };
+  const text = await Deno.readTextFile(join("./posts", `${slug}.md`));
+  const content = frontMatter<Post>(text);
+
+  return {
+    slug,
+    title: content.attributes.title,
+    publishedAt: new Date(content.attributes.publishedAt),
+    content: content.body,
+    snippet: "", // empty for now :P
+  };
 }
 
 export async function getPosts(): Promise<Post[]> {
-    const files = Deno.readDir("./posts");
-    const promises = [];
-    for await (const file of files) {
-        const slug = file.name.replace(".md", "");
-        promises.push(getPost(slug));
-    }
-    const posts = (await Promise.all(promises)) as Post[];
-    posts.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
-    return posts;
+  const files = Deno.readDir("./posts");
+  const promises = [];
+  for await (const file of files) {
+    const slug = file.name.replace(".md", "");
+    promises.push(getPost(slug));
+  }
+  const posts = (await Promise.all(promises)) as Post[];
+  posts.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
+  return posts;
 }
